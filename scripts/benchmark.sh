3d771ed760880fa896734ad5d3cf8fb31ee80ac6
Marc Zweiler <marc.zweiler@outlook.at>
2025-03-23 21:47:05 +0100

    Changed the benchmarking script to save the results of every benchmark in its own respective file

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -5,38 +5,23 @@ rm -r ./out/*
 echo "-- Building the executables..."
 ./scripts/build.sh
 
-echo "-- Benchmarking 'dima'..."
-./out/dima
+# $1 - The name of the executable to benchmark
+benchmark() {
+    echo "-- Benchmarking '$1'..."
+    mkdir -p ./test/results/test_outputs
+    touch ./test/results/test_outputs/"$1".txt
+    ./out/"$1" | tee ./test/results/test_outputs/"$1".txt
+}
 
-echo "-- Benchmarking 'dima-o1'..."
-./out/dima-o1
-
-echo "-- Benchmarking 'dima-medium'..."
-./out/dima-medium
-
-echo "-- Benchmarking 'dima-medium-o1'..."
-./out/dima-medium-o1
-
-echo "-- Benchmarking 'std-shared'..."
-./out/std-shared
-
-echo "-- Benchmarking 'std-shared-o1'..."
-./out/std-shared-o1
-
-echo "-- Benchmarking 'std-shared-medium'..."
-./out/std-shared-medium
-
-echo "-- Benchmarking 'std-shared-medium-o1'..."
-./out/std-shared-medium-o1
-
-echo "-- Benchmarking 'std-unique'..."
-./out/std-unique
-
-echo "-- Benchmarking 'std-unique-o1'..."
-./out/std-unique-o1
-
-echo "-- Benchmarking 'std-unique-medium'..."
-./out/std-unique-medium
-
-echo "-- Benchmarking 'std-unique-medium-o1'..."
-./out/std-unique-medium-o1
+benchmark "dima"
+benchmark "dima-o1"
+benchmark "dima-medium"
+benchmark "dima-medium-o1"
+benchmark "std-shared"
+benchmark "std-shared-o1"
+benchmark "std-shared-medium"
+benchmark "std-shared-medium-o1"
+benchmark "std-unique"
+benchmark "std-unique-o1"
+benchmark "std-unique-medium"
+benchmark "std-unique-medium-o1"
