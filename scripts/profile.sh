945b1fbc83ce04a3143bc976653ab220b36c8116
Marc Zweiler <marc.zweiler@outlook.at>
2025-03-27 21:01:30 +0100

    Added the ability to profile the c binary

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -9,6 +9,14 @@ err_exit() {
 # Get the projects root directory (use cd + dirname + pwd to always get absolute path, even when it is called relatively)
 root="$(cd "$(dirname "$0")" && cd .. && pwd)"
 
+# Check if to test dima or dima-c
+if [ "$1" = "c" ]; then
+    executable="$root/out/dima-c"
+    shift
+else
+    executable="$root/out/dima"
+fi
+
 # Make sure that 'perf' is installed
 [ ! -x "$(command -v perf)" ] && err_exit 1 "'perf' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
 
@@ -18,8 +26,8 @@ root="$(cd "$(dirname "$0")" && cd .. && pwd)"
 # Make sure that 'stackcollapse-perf.pl' is installed
 [ ! -x "$(command -v stackcollapse-perf.pl)" ] && err_exit 1 "'stackcollapse-perf.pl' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
 
-# Make sure that the executable (main) exists in the "$root/" directory
-[ ! -x "$root/out/dima" ] && err_exit 1 "Executable 'dima' not found in '$root/out/' directory!"
+# Make sure that the executable exists
+[ ! -x "$executable" ] && err_exit 1 "Executable '$executable' not found!"
 
 # Make sure that cli arguments are given
 # [ "$#" -eq 0 ] && err_exit 1 "No cli arguments given!"
@@ -32,10 +40,10 @@ echo "-- Starting Profiling"
 
 # Collect performance data with perf and forward all cli arguments of this script
 # shellcheck disable=SC2068 # We want to forward all arguments
-sudo perf record --call-graph dwarf --all-user -F 9999 -g -o "$root/scripts/profiling/perf.data" "$root/out/dima" $@
+sudo perf record --call-graph dwarf --all-user -F 9999 -g -o "$root/scripts/profiling/perf.data" "$executable" $@
 
 # Generate the svg file
-sudo perf script -i "$root/scripts/profiling/perf.data" | stackcollapse-perf.pl | flamegraph.pl > "$root/scripts/profiling/out.svg"
+sudo perf script -i "$root/scripts/profiling/perf.data" | stackcollapse-perf.pl | flamegraph.pl >"$root/scripts/profiling/out.svg"
 
 # Visualize the flamegraph
 firefox "$root/scripts/profiling/out.svg"
